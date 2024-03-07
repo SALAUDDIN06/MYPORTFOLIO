cd80cf9c6a5a8497b21f7dfbcab9734b10424478
SALAUDDIN06 <mohammadsallu2004@gmail.com>
2024-03-07 06:47:40 +0530

    updated sitePopers info

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
+  name: "MOHAMMED SALAUDDIN",
+  title: "Data Analyst & Web Designer",
+  email: "salauddinmohammed902@gmail.com",
+  gitHub: "https://github.com/SALAUDDIN06",
+  instagram: "https://www.instagram.com/salauddin_20_/?next=%2F&hl=en",
+  linkedIn: "https://www.linkedin.com/in/mohammed-salauddin-223804266",
+  medium: "English",
+  twitter: "https://twitter.com/MusicalAnime1",
   youTube: "Code",
 };
 
